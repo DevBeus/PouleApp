fb1fd2345aab53bac9668380480199cd5556e53b
DevBeus <elmar118@gmail.com>
2017-11-18 17:51:34 +0100

    Beautified ranking table and table scheme

--- app/src/main/java/com/example/pouleapp/Activities/TabSchemeTable.java
+++ app/src/main/java/com/example/pouleapp/Activities/TabSchemeTable.java
@@ -2,10 +2,12 @@ package com.example.pouleapp.Activities;
 
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.res.ResourcesCompat;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -63,77 +65,77 @@ public class TabSchemeTable extends Fragment {
 
         TableLayout tl = (TableLayout) v.findViewById(R.id.tab_scheme_table_layout_table);
 
-        for (int i=0; i < teamList.size()+1; i++) {
-            TableRow tr = new TableRow(v.getContext());
-            TextView tv = new TextView(v.getContext());
-            tv.setMaxLines(1);
-            setTextViewAttributes(tv);
+            for (int i = 0; i < teamList.size() + 1; i++) {
+                TableRow tr = new TableRow(v.getContext());
+                TextView tv = new TextView(v.getContext());
+                tv.setMaxLines(1);
+                setTextViewAttributes(tv);
 
-            if (i==0) {
-                tv.setText(""); //Top left remains empty: vertically home team, horizontally against team
-                tv.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.colorYellow, null)); //null means no theme
-            }
-            else {
-                tv.setText(teamList.get(i-1).getTeamName()); // first column is home team
-            }
-
-            tr.addView(tv);
-
-            for (int j=0; j < teamList.size()+1; j++) {
-                TextView tv1 = new TextView(v.getContext());
-                tv1.setMaxLines(1);
-                setTextViewAttributes(tv1);
-
-                if (j==0) {
-                    //skip first column
+                if (i == 0) {
+                    tv.setText(""); //Top left remains empty: vertically home team, horizontally against team
+                    tv.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.colorYellow, null)); //null means no theme
                 } else {
-                    if (i==0) {
-                        // first row consists of team names
-                        tv1.setText(teamList.get(j-1).getTeamName());
-                        tv1.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.colorYellow, null)); //null means no theme
-                    } else if (i==j) {
-                        // cells on the diagonal become gray
-                        tv1.setText("");
-                        tv1.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.colorGray, null)); //null means no theme
-                    }
-                    else {
-                        String t = pouleScheme.getMatchResult(i-1,j-1);
-                        tv1.setText(t);
-                        tv1.setClickable(true);
-
-                        final int x = i-1;
-                        final int y = j-1;
-
-                        tv1.setOnClickListener(new View.OnClickListener(){
-                            public void onClick(View v) {
-                                Intent intent = new Intent(v.getContext(), EditMatchActivity.class);
-                                intent.putExtra(PREVIOUS_ACTIVITY,SCHEME_TABLE_ACTIVITY);
-                                intent.putExtra(PREVIOUS_TAB,SCHEMETABLE_TAB);
-                                intent.putExtra(POULE_INDEX,mPoule_Index);
-                                intent.putExtra(SCHEME_ROW,x);
-                                intent.putExtra(SCHEME_COLUMN,y);
-                                startActivity(intent);
-                            }
-                        });
-
-                    }
-
-                    tr.addView(tv1);
+                    tv.setText(teamList.get(i - 1).getTeamName()); // first column is home team
+                    tv.setGravity(Gravity.START);
                 }
 
+                tr.addView(tv);
+
+                for (int j = 0; j < teamList.size() + 1; j++) {
+                    TextView tv1 = new TextView(v.getContext());
+                    tv1.setMaxLines(1);
+                    setTextViewAttributes(tv1);
+
+                    if (j == 0) {
+                        //skip first column
+                    } else {
+                        if (i == 0) {
+                            // first row consists of team names
+                            tv1.setText(teamList.get(j - 1).getTeamName());
+                            tv1.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.colorYellow, null)); //null means no theme
+                        } else if (i == j) {
+                            // cells on the diagonal become gray
+                            tv1.setText("");
+                            tv1.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.colorGray, null)); //null means no theme
+                        } else {
+                            String t = pouleScheme.getMatchResult(i - 1, j - 1);
+                            tv1.setText(t);
+                            tv1.setClickable(true);
+
+                            final int x = i - 1;
+                            final int y = j - 1;
+
+                            tv1.setOnClickListener(new View.OnClickListener() {
+                                public void onClick(View v) {
+                                    Intent intent = new Intent(v.getContext(), EditMatchActivity.class);
+                                    intent.putExtra(PREVIOUS_ACTIVITY, SCHEME_TABLE_ACTIVITY);
+                                    intent.putExtra(PREVIOUS_TAB, SCHEMETABLE_TAB);
+                                    intent.putExtra(POULE_INDEX, mPoule_Index);
+                                    intent.putExtra(SCHEME_ROW, x);
+                                    intent.putExtra(SCHEME_COLUMN, y);
+                                    startActivity(intent);
+                                }
+                            });
+
+                        }
+
+                        tr.addView(tv1);
+                    }
+
+                }
+
+                tl.addView(tr);
+
             }
 
-            tl.addView(tr);
-
-        }
 
         return v;
     }
 
     private void setTextViewAttributes(TextView tv) {
-        tv.setBackgroundResource(R.drawable.cell_shape);
         tv.setEllipsize(TextUtils.TruncateAt.END);
         tv.setPadding(5,5,5,5);
+        tv.setGravity(Gravity.CENTER_HORIZONTAL);
 
         tv.setLayoutParams(new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f));
 
